a2b8925f880138f754db54dbfc97262c4e9d53a6
abhishekabhi123 <vadakkemadamabhishek@gmail.com>
2023-01-06 23:28:38 +0530

    changed to production url

--- client/src/axios.js
+++ client/src/axios.js
@@ -9,7 +9,7 @@ const header = localStorage.getItem("accessToken")
     };
 
 const instance = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://socialista.onrender.com/api",
   headers: header,
   withCredentials: true,
 });
