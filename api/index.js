e85be3909b8220d6be44b66169f5c63ff42241c3
abhishekabhi123 <vadakkemadamabhishek@gmail.com>
2023-01-14 12:04:25 +0530

    admin cors

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://api.liansocialmedia.ml", "https://liansocialmedia.ml"],
+    origin: ["https://admin.liansocialmedia.ml", "https://liansocialmedia.ml"],
   })
 );
 app.use(cookieParser());
